cbec205f5aace2279afc6928f54a8ab40f2694b2
Wojciech Urbanski <teh.urb@gmail.com>
2015-10-17 20:06:21 +0200

    SOK-14 Poprawki w kontrolerze tasków

--- frontend/app/scripts/controllers/tasks.js
+++ frontend/app/scripts/controllers/tasks.js
@@ -13,8 +13,33 @@ angular.module('sokApp')
   function ($http, $routeParams, $window, SokApi) {
     var vm = this;
 
-    vm.error = false;
-    vm.ready = true;
+    function saveAnswer() {
+      SokApi.answer.update({content: vm.currentAnswer.content, candidateId: vm.user.id, taskId: vm.taskList[vm.selected].id});
+    }
+
+    function getAnswer(task_id) {
+      vm.currentAnswer = SokApi.answer.get({taskId: task_id, token: $routeParams.token});
+      vm.answerForm.$setPristine();
+      vm.oldVal = angular.copy(vm.selected);
+    }
+
+    function confirmChange() {
+      if (vm.answerForm.taskAnswer.$dirty && !confirm('lose unsaved changes?')) {
+        vm.selected = vm.oldVal;
+      } else {
+        getAnswer(vm.taskList[vm.selected].id);
+      }
+    }
+
+    function init() {
+      vm.user = SokApi.user.get({token: $routeParams.token});
+      vm.taskList = SokApi.tasks.query();
+      vm.taskList.$promise
+      .then(function(data){
+        getAnswer(data[vm.selected].id);
+      });
+    }
+
     vm.user = null;
     vm.taskList = null;
     vm.currentAnswer = null;
@@ -25,32 +50,5 @@ angular.module('sokApp')
     vm.confirmChange = confirmChange;
 
     init();
-
-    function saveAnswer() {
-      SokApi.answer.update({content: vm.currentAnswer.content, candidateId: vm.user.id, taskId: vm.taskList[vm.selected].id})
-    }
-
-    function init() {
-      vm.user = SokApi.user.get({token: $routeParams.token});
-      vm.taskList = SokApi.tasks.query();
-      vm.taskList.$promise
-      .then(function(data){
-        getAnswer(data[vm.selected].id)
-      })
-    }
-
-    function getAnswer(task_id) {
-      vm.currentAnswer = SokApi.answer.get({taskId: task_id, token: $routeParams.token});
-      vm.answerForm.taskAnswer.$dirty = false;
-      vm.oldVal = angular.copy(vm.selected);
-    }
-
-    function confirmChange() {
-      if (vm.answerForm.taskAnswer.$dirty && !confirm('lose unsaved changes?')) {
-        vm.selected = vm.oldVal;
-      } else {
-        getAnswer(vm.taskList[vm.selected].id);
-      }
-    };
 
   }]);
