d1161cd4b5e2a286429004dd5b77ea79fc45a186
alan <alan@malpiszon.net>
2015-10-17 22:17:54 +0200

    SOK-7 Poziomy trudności zadań

--- backend/src/main/java/pl/gda/pg/ds/sok/entities/Task.java
+++ backend/src/main/java/pl/gda/pg/ds/sok/entities/Task.java
@@ -4,13 +4,7 @@ import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
 
-import javax.persistence.CascadeType;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.OneToMany;
-import javax.persistence.Table;
+import javax.persistence.*;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
@@ -35,10 +29,10 @@ public class Task implements Serializable  {
 	@Type(type="text")
 	private String content;
     @NotNull
-    @Type(type="integer")
+	@Column(columnDefinition = "smallint default 1")
     private Integer difficulty;
 	@OneToMany(mappedBy="task",cascade=CascadeType.PERSIST)
-    private List<Answer> answers = new ArrayList<Answer>();
+    private List<Answer> answers = new ArrayList<>();
 	
 	public Task() {
 	}
