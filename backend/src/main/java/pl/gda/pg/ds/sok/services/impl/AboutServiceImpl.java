9b9272234e691b64ea1a4fbcb310ddd3c22c8c9c
alan <alan@malpiszon.net>
2015-11-12 21:38:41 +0100

    SOK-42 Budowanie .war-a dla środowiska produkcyjnego i testowego

---
+++ backend/src/main/java/pl/gda/pg/ds/sok/services/impl/AboutServiceImpl.java
@@ -0,0 +1,27 @@
+package pl.gda.pg.ds.sok.services.impl;
+
+import com.google.common.collect.Maps;
+import org.apache.log4j.Logger;
+import pl.gda.pg.ds.sok.services.AboutService;
+import pl.gda.pg.ds.sok.utils.PropertiesUtil;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import java.util.Map;
+
+@Path("/about")
+public class AboutServiceImpl implements AboutService {
+	
+	private static final Logger logger = Logger.getLogger(AboutServiceImpl.class);
+
+	@GET
+	@Produces(MediaType.APPLICATION_JSON)
+	public Response getAbout() {
+		Map<String,String> about = Maps.newHashMap();
+		about.put("build", PropertiesUtil.getProperty("app.build"));
+		return Response.ok(about).build();
+	}
+}
