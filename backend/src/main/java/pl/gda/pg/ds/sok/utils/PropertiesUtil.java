f713a9403a82c3ba9f2f42203302cf5787a774b5
alan <alan@malpiszon.net>
2015-11-20 23:17:59 +0100

    SOK-48 Błędy w mailu wysyłanym ze środowiska dev - kodowanie

--- backend/src/main/java/pl/gda/pg/ds/sok/utils/PropertiesUtil.java
+++ backend/src/main/java/pl/gda/pg/ds/sok/utils/PropertiesUtil.java
@@ -7,19 +7,19 @@ import java.util.Properties;
 import org.apache.log4j.Logger;
 
 public class PropertiesUtil {
-	
+
 	private static final Logger logger = Logger.getLogger(PropertiesUtil.class);
 	private static final String FILE = "config.properties";
-	
+
 	public static String getProperty(String propertyName) {
-	
+
 		ClassLoader loader = Thread.currentThread().getContextClassLoader();
 		InputStream inputStream = loader.getResourceAsStream(FILE);
 		if (inputStream != null) {
 			Properties properties = new Properties();
 			try {
 	            properties.load(inputStream);
-	            return new String(properties.getProperty(propertyName).getBytes("ISO-8859-1"), "UTF-8");
+	            return new String(properties.getProperty(propertyName).getBytes("ISO-8859-1"));
 	        } catch (IOException e) {
 	        	logger.fatal("Problem reading properties file", e);
 	        }
