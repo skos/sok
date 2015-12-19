2b8585ba67d4ebb3c9b2d739b1f1007419d86858
alan <alan@ds.pg.gda.pl>
2015-12-19 11:46:43 +0100

    SOK-57 Nie wysyłają się maile na dev/prod - debug

--- backend/src/main/java/pl/gda/pg/ds/sok/services/impl/AboutServiceImpl.java
+++ backend/src/main/java/pl/gda/pg/ds/sok/services/impl/AboutServiceImpl.java
@@ -5,12 +5,18 @@ import org.apache.log4j.Logger;
 import pl.gda.pg.ds.sok.services.AboutService;
 import pl.gda.pg.ds.sok.utils.PropertiesUtil;
 
+import javax.mail.Message;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import java.util.Map;
+import java.util.Properties;
 
 @Path("/about")
 public class AboutServiceImpl implements AboutService {
@@ -23,7 +29,22 @@ public class AboutServiceImpl implements AboutService {
 		Map<String,String> about = Maps.newHashMap();
 		about.put("build", PropertiesUtil.getProperty("app.build"));
 		about.put("version", PropertiesUtil.getProperty("app.version"));
-		about.put("mailserver", PropertiesUtil.getProperty("mail.server"));
+
+		Properties properties = new Properties();
+		properties.setProperty("mail.smtp.host", PropertiesUtil.getProperty("mail.server"));
+		properties.put("mail.debug", "true");
+		Session session = Session.getDefaultInstance(properties);
+
+		logger.error(properties.getProperty("mail.smtp.host"));
+
+		MimeMessage msg = new MimeMessage(session);
+		msg.setFrom(new InternetAddress(PropertiesUtil.getProperty("mail.addressFrom"), PropertiesUtil.getProperty("mail.from")));
+		msg.addRecipient(Message.RecipientType.TO, new InternetAddress(mailTo, recipientName));
+		msg.setSubject(subject);
+		msg.setText(body, "UTF-8");
+		Transport.send(msg);
+
+
 		return Response.ok(about).build();
 	}
 }
