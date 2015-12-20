4451b65791ca05f2f66ea0da56dedb3bef8e216e
bszafran <bartek@ds.pg.gda.pl>
2015-12-20 14:41:33 +0100

    Revert "Revert "Merge remote-tracking branch 'origin/master'""
    This reverts commit a554c73dd3558609ed8a544d9a255565efd29109.

--- frontend/app/scripts/controllers/tasks.js
+++ frontend/app/scripts/controllers/tasks.js
@@ -20,29 +20,41 @@ angular.module('sokApp')
       });
     }
 
+    function getRating(task_id) {
+      SokApi.rating.get({taskId: task_id, token: vm.user.token }).$promise
+      .then( function(data) {
+        vm.latestRating = data;
+      }, function() {
+        vm.latestRating = null;
+      })
+    }
+
     function getAnswer(task_id) {
       vm.currentAnswer = SokApi.answer.get({taskId: task_id, token: vm.user.token});
       if (vm.answerForm) { vm.answerForm.$setPristine(); }
       vm.oldSelected = vm.selected; // keep reference to first-selected item
     }
 
-    function confirmChange() {
+    function changeTask() {
       if (vm.answerForm.taskAnswer.$dirty && !confirm('Przechodząc dalej, stracisz niezapisane zmiany. Chcesz kontynuować?')) {
         vm.selected = vm.oldSelected;
       } else {
         getAnswer(vm.selected.id);
+        getRating(vm.selected.id);
       }
     }
 
     vm.user = resourceData.user;
     vm.taskList = resourceData.taskList;
+    vm.latestRating = null;
     vm.currentAnswer = null;
-    vm.selected = vm.taskList[0];
     vm.oldSelected = null;
     vm.introCollapsed = false;
+    vm.selected = vm.taskList[0];
     vm.getAnswer = getAnswer;
     vm.saveAnswer = saveAnswer;
-    vm.confirmChange = confirmChange;
+    vm.changeTask = changeTask;
     getAnswer(vm.selected.id);
+    getRating(vm.selected.id);
 
   }]);
